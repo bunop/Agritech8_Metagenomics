ae76f65458b6db13673837a916ee98ce31e45cbb
Paolo Cozzi <bunop@libero.it>
2025-12-12 14:03:37 +0100

    :sparkles: add batch scripts jobs

---
+++ scripts/november_2023-bacteria.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#SBATCH --job-name=november_2023-bacteria
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=32gb
+#SBATCH --output=november_2023-bacteria-%j.out
+
+eval "$(conda shell.bash hook)"
+conda activate R-4.5
+
+export TAR_PROJECT="november_2023-bacteria"
+
+# execute the tar_make command
+if ! Rscript -e "targets::tar_make()"; then
+    echo "Error: tar_make command failed."
+    exit 1
+fi
+
+# prune the targets
+if ! Rscript -e "targets::tar_prune()"; then
+    echo "Error: tar_prune command failed."
+    exit 1
+fi
